d8362dc60873360a17646e4119b9239a818192d4
Ula Jeziorna <jeziornaula@wp.pl>
2021-12-09 20:13:27 +0100

    repo zdalne

---
+++ js/script.js
@@ -0,0 +1,165 @@
+let button = document.querySelector(`button`);
+console.log(button);
+
+const randomGenerate = (min, max) => (Math.floor(Math.random() * (max - min)) + min)
+
+
+// const chunkArray = (array, n) => {
+
+//   let chunkLength = Math.max(array.length / n, 1);
+//   // console.log(chunkLength);
+//   var chunks = [];
+//   // console.log(chunks);
+//   for (var i = 0; i < n; i++) {
+//     if (chunkLength * (i + 1) <= array.length) {
+
+//       chunks.push(array.slice(chunkLength * i, chunkLength * (i + 1)))
+//     }
+//   }
+//   return chunks;
+// }
+
+
+
+
+const compareNumbers = (a, b) => {
+  return a - b
+}
+
+
+
+button.onclick = () => {
+  // console.log(`click dziala`)
+
+
+  // Removing the DOMElement from the Stage will not affect the related html element it wraps. DOMElement is useful for controlling position, transformation, and visibility of an HTML element, but if you remove it from the stage, the html element is not affected, since the element is never really on the stage in the first place.
+  let removeListEvenDiv = document.querySelector(`.evenNumbers`)
+  // console.log(removeListEvenDiv);
+  let removeListOddDiv = document.querySelector(`.oddNumbers`)
+  // console.log(removeListOddDiv);
+
+
+  let remowedElement1 = document.querySelector(`.ulEvenNumbers`)
+  let remowedElement2 = document.querySelector(`.ulOddNumbers`)
+
+  removeListEvenDiv.removeChild(remowedElement1)
+  removeListOddDiv.removeChild(remowedElement2)
+
+
+
+  // removeListEven.parentNode.removeChild(removeListEven)
+
+
+
+
+  let arrEvenSortedNumbers = [];
+  let arrOddSortedNumbers = [];
+
+  // tworzenie tablicy z randomowymi liczbami 20
+  let random = []
+  do {
+    random.push(randomGenerate(1, 101))
+  } while (random.length < 20)
+
+  // console.log(random);
+
+  let sortedRandom = random.sort(compareNumbers);
+  console.log(sortedRandom);
+
+  for (let i = 0; i < sortedRandom.length; i++) {
+    // console.log(sortedRandom[i]);
+
+    if (sortedRandom[i] % 2 === 0) {
+      arrEvenSortedNumbers.push(sortedRandom[i])
+    } else {
+      arrOddSortedNumbers.push(sortedRandom[i])
+    }
+  }
+
+  console.log(arrEvenSortedNumbers);
+  console.log(arrOddSortedNumbers);
+
+
+  // Pamiętaj gamoniu!!!! za pomocą querySelector!!! Nie przez id lub class. Nie dziala wtedy.
+  let divEvenNumbers = document.querySelector(`.even-numbers`)
+  // console.log(divEvenNumbers);
+  let divOddNumbers = document.querySelector(`.odd-numbers`)
+  // // console.log(divOddNumbers);
+
+  let ulEvenNumbers = document.createElement(`ul`)
+  // console.log(ulEvenNumbers);
+  ulEvenNumbers.className = `ulEvenNumbers`;
+  let ulOddNumbers = document.createElement(`ul`)
+  ulOddNumbers.className = `ulOddNumbers`;
+
+  arrEvenSortedNumbers.forEach(evenNumber => {
+    let newLiElem = document.createElement(`li`)
+    newLiElem.className = `numbers-list-item`
+    newLiElem.innerText = evenNumber
+
+    ulEvenNumbers.appendChild(newLiElem)
+  });
+
+
+  arrOddSortedNumbers.forEach(oddNumber => {
+    let newLiElem = document.createElement(`li`)
+    newLiElem.className = `numbers-list-item`
+    newLiElem.innerText = oddNumber
+
+    ulOddNumbers.appendChild(newLiElem)
+  });
+
+  divEvenNumbers.appendChild(ulEvenNumbers)
+  divOddNumbers.appendChild(ulOddNumbers)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  Lamanie tablicy
+// function chunkArray(arr, n) {
+//   var chunkLength = Math.max(arr.length / n, 1);
+//   var chunks = [];
+//   for (var i = 0; i < n; i++) {
+//     if (chunkLength * (i + 1) <= arr.length) chunks.push(arr.slice(chunkLength * i, chunkLength * (i + 1)));
+//   }
+//   return chunks;
+// }
+
+// console.log(chunkArray([1, 2, 3, 4, 5, 6], 2));
+// console.log(chunkArray([1, 2, 3, 4, 5, 6, 7, 8], 3));
+
+
+
+
