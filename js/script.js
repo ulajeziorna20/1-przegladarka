b76c37b7bcf464f37665a7efcb5c6e1853d8678f
Krystian Dziopa <krystian.dziopa@ilustratum.pl>
2021-12-09 21:32:39 +0100

    Brand new version

--- js/script.js
+++ js/script.js
@@ -1,108 +1,101 @@
-let button = document.querySelector(`button`) //zmienna ma być btnGenerateNumber
-console.log(button)
-
-const randomGenerate = (min, max) => {
-  return Math.floor(Math.random() * (max - min)) + min //funkcja ma mieć blok kodu i return
+const generateRandomNum = (min, max) => {
+  return Math.floor(Math.random() * (max - min)) + min
 }
 
-// const compareNumbers = (a, b) => {
-//   return a - b
-// }
+const createElement = (elType, elAttrs, elProps) => {
+  const newEl = document.createElement(elType)
 
-button.onclick = () => {
-
-
-  // zmień na addEventListener
-  // // console.log(`click dziala`)
-
-  // // Removing the DOMElement from the Stage will not affect the related html element it wraps. DOMElement is useful for controlling position, transformation, and visibility of an HTML element, but if you remove it from the stage, the html element is not affected, since the element is never really on the stage in the first place.
-  // let removeListEvenDiv = document.querySelector(`.evenNumbers`)
-  // // console.log(removeListEvenDiv);
-  // let removeListOddDiv = document.querySelector(`.oddNumbers`)
-  // // console.log(removeListOddDiv);
-
-  // let remowedElement1 = document.querySelector(`.ulEvenNumbers`)
-  // let remowedElement2 = document.querySelector(`.ulOddNumbers`)
-
-  // // removeListEvenDiv.removeChild(remowedElement1)
-  // // removeListOddDiv.removeChild(remowedElement2)
-
-  // // removeListEven.parentNode.removeChild(removeListEven)
-
-  let arrEvenSortedNumbers = []
-  let arrOddSortedNumbers = []
-
-  // tworzenie tablicy z randomowymi liczbami 20
-  let random = []
-  do {
-    random.push(randomGenerate(1, 101))
-  } while (random.length < 20)
-
-  // console.log(random);
-
-  let sortedRandom = random.sort((a, b) => {
-    return a - b
-  })
-  // console.log(sortedRandom)
-
-  for (let i = 0; i < sortedRandom.length; i++) { // zrobić wszystko w jedej pętli
-    // console.log(sortedRandom[i]);
-
-    if (sortedRandom[i] % 2 === 0) {
-      arrEvenSortedNumbers.push(sortedRandom[i])
-    } else {
-      arrOddSortedNumbers.push(sortedRandom[i])
+  for (const key in elAttrs) {
+    if (Object.hasOwnProperty.call(elAttrs, key)) {
+      newEl.setAttribute(key, elAttrs[key])
     }
   }
 
-  console.log(arrEvenSortedNumbers)
-  console.log(arrOddSortedNumbers)
+  for (const key in elProps) {
+    if (Object.hasOwnProperty.call(elProps, key)) {
+      newEl[key] = elProps[key]
+    }
+  }
 
-  // Pamiętaj gamoniu!!!! za pomocą querySelector!!! Nie przez id lub class. Nie dziala wtedy.
-  let divEvenNumbers = document.querySelector(`.even-numbers`)
-  // console.log(divEvenNumbers);
-  let divOddNumbers = document.querySelector(`.odd-numbers`)
-  // // console.log(divOddNumbers);
-
-  divEvenNumbers.innerHTML = '<span>Your Even Numbers!</span>';
-  divOddNumbers.innerHTML = '<span>Your Odd Numbers!</span>'
-
-
-  let ulEvenNumbers = document.createElement(`ul`)
-  // console.log(ulEvenNumbers);
-  ulEvenNumbers.className = `ulEvenNumbers`
-  let ulOddNumbers = document.createElement(`ul`)
-  ulOddNumbers.className = `ulOddNumbers`
-
-  arrEvenSortedNumbers.forEach((evenNumber) => {
-    let newLiElem = document.createElement(`li`)
-    newLiElem.className = `numbers-list-item`
-    newLiElem.innerText = evenNumber
-
-    ulEvenNumbers.appendChild(newLiElem)
-  })
-
-  arrOddSortedNumbers.forEach((oddNumber) => {
-    let newLiElem = document.createElement(`li`)
-    newLiElem.className = `numbers-list-item`
-    newLiElem.innerText = oddNumber
-
-    ulOddNumbers.appendChild(newLiElem)
-  })
-
-  divEvenNumbers.appendChild(ulEvenNumbers)
-  divOddNumbers.appendChild(ulOddNumbers)
+  return newEl
 }
 
-//  Lamanie tablicy
-// function chunkArray(arr, n) {
-//   var chunkLength = Math.max(arr.length / n, 1);
-//   var chunks = [];
-//   for (var i = 0; i < n; i++) {
-//     if (chunkLength * (i + 1) <= arr.length) chunks.push(arr.slice(chunkLength * i, chunkLength * (i + 1)));
-//   }
-//   return chunks;
-// }
+// create main container
+const mainContainer = createElement(
+  'section',
+  { class: 'container', id: 'mainContainer' },
+  {}
+)
+document.body.prepend(mainContainer)
 
-// console.log(chunkArray([1, 2, 3, 4, 5, 6], 2));
-// console.log(chunkArray([1, 2, 3, 4, 5, 6, 7, 8], 3));
+// create button
+let btnGenerateNumbers = createElement(
+  'button',
+  { class: 'btnGenerateNumbers', id: 'btnGenerateNumbers' },
+  { innerText: 'Generate Numbers' }
+)
+document.body.prepend(btnGenerateNumbers)
+
+btnGenerateNumbers.addEventListener('click', () => {
+  let arrEvenNumbers = []
+  let arrOddNumbers = []
+
+  // generate 20 random numbers
+  for (let i = 0; i < 20; i++) {
+    const randomNum = generateRandomNum(1, 100)
+
+    const even = randomNum % 2 === 0
+
+    if (even) {
+      arrEvenNumbers.push(randomNum)
+    } else {
+      arrOddNumbers.push(randomNum)
+    }
+  }
+
+  // sort arrays
+  arrEvenNumbers.sort()
+  arrOddNumbers.sort()
+
+  // check if divEvenNumbers && divOddNumbers exist and remove them if so ;)
+  let divEvenNumbers = document.querySelector('#evenNumbersContainer')
+  if (divEvenNumbers) {
+    divEvenNumbers.remove()
+  }
+  let divOddNumbers = document.querySelector('#oddNumbersContainer')
+  if (divOddNumbers) {
+    divOddNumbers.remove()
+  }
+
+  // create even numbers container & list
+  divEvenNumbers = createElement(
+    'div',
+    { class: 'evenNumbersContainer', id: 'evenNumbersContainer' },
+    { innerHTML: '<span>Your Even Numbers!</span>' }
+  )
+  mainContainer.appendChild(divEvenNumbers)
+  const ulEvenNumbers = createElement(
+    'ul',
+    { class: 'evenNumbersList', id: 'evenNumbersList' },
+    {}
+  )
+  divEvenNumbers.appendChild(ulEvenNumbers)
+
+  // create odd numbers container
+  divOddNumbers = createElement(
+    'div',
+    { class: 'oddNumbersContainer', id: 'oddNumbersContainer' },
+    { innerHTML: '<span>Your Odd Numbers!</span>' }
+  )
+  mainContainer.appendChild(divOddNumbers)
+
+  // create elements of even numbers and add it to dom
+  for (const num of arrEvenNumbers) {
+    // const
+  }
+
+  // create elements of odd numbers and add it to dom
+  for (const num of arrOddNumbers) {
+    // const
+  }
+})
